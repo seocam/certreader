7654c4a28dd17234016dc608758fd515a0559c5f
Sergio Oliveira Campos <seocam@redhat.com>
2020-07-01 13:23:33 -0300

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="certreader",
-    version="0.1.0",
+    version="0.1.1",
     author="Sergio Oliveira Campos",
     author_email="seocam@redhat.com",
     description="Yet another x509 certificate parser.",
